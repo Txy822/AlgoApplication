35bce2b58d5d53cd06a7b1fdb4587a3fecbb3d23
tyohanis-wolleli <tyohanis2018@gmail.com>
2021-07-21 18:12:34 +0100

    cleaning

--- src/sample/AlgoAppController.java
+++ src/sample/AlgoAppController.java
@@ -10,15 +10,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
 import java.util.*;
 
